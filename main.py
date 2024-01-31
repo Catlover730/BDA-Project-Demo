b739cf3f70d0045c649908a7bf04f38f512e2579
catlover <chenhong@u.nus.edu>
2024-01-31 12:50:43 +0800

    Add a database demo, add output ACC, add amounts of epochs

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:979306860773ff8e5019a92afacb4d684b4ec5f2bc925f5175ae5879bf47ec74
-size 3882
+oid sha256:a8641e5427eaad8d4ff4df02d59dc90c5c5213228f882d9da74ebb3b8d37d325
+size 4002
