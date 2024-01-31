3e4d1cfabe6fbdcff1d30489d48831956e670d4a
catlover <chenhong@u.nus.edu>
2024-01-31 15:41:19 +0800

    Optimize the structure of code

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a8641e5427eaad8d4ff4df02d59dc90c5c5213228f882d9da74ebb3b8d37d325
-size 4002
+oid sha256:6fda1f2ff13c405024b5b914338bae080c6b511328528a494facd93e8a1fd0b4
+size 4236
